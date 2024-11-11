c469f3b3893d956dff29cb105be4fc6e7bb029bc
jinoo <jinoo0306@naver.com>
2024-11-11 14:49:35 +0900

    feat(1) : 메세지 상수들 미리 정의

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const INPUT_MESSAGES = {
+  ITEM_QUANTITY_INPUT:
+    "\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n",
+};
+
+export const ERROR_MESSAGES = {
+  ERROR_PREFIX: "[ERROR]",
+  INVALID_FORMAT: "잘못된 형식입니다. 다시 입력해 주세요.",
+  EXCEED_STOCK: "재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.",
+};
+
+export const WELCOME_MESSAGE = {
+  GREETING_WSTORE: "안녕하세요. W편의점입니다.",
+  PRODUCT_INFO: "현재 보유하고 있는 상품입니다.\n",
+};
